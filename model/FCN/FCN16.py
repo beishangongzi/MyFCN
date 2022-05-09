5f1a7811037aabdea04f56edc2ec24326bc8c4ed
Andy Z Wright <zhangruibin021@gmail.com>
2022-05-09 14:45:36 +0800

    change FCN8s and FCN16s

--- model/FCN/FCN16.py
+++ model/FCN/FCN16.py
@@ -7,11 +7,26 @@ class FCN16(nn.Module):
     def forward(self, x):
         feature_32 = self.features(x)["pool32"]
         feature_16 = self.features(x)["pool16"]
-        y = self.up_sampling(feature_32, feature_16, 512)
-        y = self.up_sampling(y, None, 256)
-        y = self.up_sampling(y, None, 128)
-        y = self.up_sampling(y, None, 64)
-        y = self.up_sampling(y, None, 32)
+
+        y = self.deconv1(feature_32)
+        y = self.relu(y)
+        y = self.bn1(y + feature_16)
+
+        y = self.deconv2(y)
+        y = self.relu(y)
+        y = self.bn2(y)
+
+        y = self.deconv3(y)
+        y = self.relu(y)
+        y = self.bn3(y)
+
+        y = self.deconv4(y)
+        y = self.relu(y)
+        y = self.bn4(y)
+
+        y = self.deconv5(y)
+        y = self.relu(y)
+        y = self.bn5(y)
         y = self.classifier(y)
         return y
 
--- model/FCN/FCN8.py
+++ model/FCN/FCN8.py
@@ -9,12 +9,25 @@ class FCN8(nn.Module):
         feature_16 = self.features(x)["pool16"]
         feature_8 = self.features(x)["pool8"]
 
-        y = self.up_sampling(feature_32, feature_16, 512)
-        y = self.up_sampling(y, feature_8, 256)
-        y = self.up_sampling(y, None, 128)
-        y = self.up_sampling(y, None, 64)
-        y = self.up_sampling(y, None, 32)
-        y = self.classifier(y)
+        y = self.deconv1(feature_32)
+        y = self.relu(y)
+        y = self.bn1(y + feature_16)
+
+        y = self.deconv2(y)
+        y = self.relu(y)
+        y = self.bn2(y + feature_8)
+
+        y = self.deconv3(y)
+        y = self.relu(y)
+        y = self.bn3(y)
+
+        y = self.deconv4(y)
+        y = self.relu(y)
+        y = self.bn4(y)
+
+        y = self.deconv5(y)
+        y = self.relu(y)
+        y = self.bn5(y)
         y = self.classifier(y)
         return y
 
