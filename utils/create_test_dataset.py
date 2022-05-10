c9b3a7cdd6f75f32486c733716be043adcc2d627
Andy Z Wright <zhangruibin021@gmail.com>
2022-05-10 21:55:00 +0800

    add create dataset py

--- utils/create_test_dataset.py
+++ utils/create_test_dataset.py
@@ -4,21 +4,57 @@ import os
 import shutil
 
 import numpy as np
+def mkdir(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+
+def mkdir_list(dirs: list):
+    for dir_item in dirs:
+        mkdir(dir_item)
+
+
+def move_files(src, dist):
+    if not os.path.exists(src):
+        return
+    for f in os.listdir(src):
+        if not f.endswith("npy"):
+            continue
+        shutil.move(os.path.join(src, f), os.path.join(dist, f))
+
 
 os.chdir("../data/obt")
-ls = np.array(os.listdir("image"))
-np.random.shuffle(ls)
-ls = ls[:20]
-if not  os.path.exists("testImagePreds"):
-    os.mkdir("testImagePreds")
 
-if not os.path.exists("testImageMasks"):
-    os.mkdir("testImageMasks")
-if not os.path.exists("testImage"):
-    os.mkdir("testImage")
+
+testImagePreds = "testImagePreds"
+testImageMasks = "testImageMasks"
+testImage = "testImage"
+testImageMorphology = "testImageMorphology"
+erode = os.path.join(testImageMorphology, "erode")
+dilate = os.path.join(testImageMorphology, "dilate")
+open_ = os.path.join(testImageMorphology, "open")
+close = os.path.join(testImageMorphology, "close")
+image = "image"
+imageMask = "imageMasks"
+move_files(testImage, image)
+move_files(testImageMasks, imageMask)
+
+
+shutil.rmtree(testImage, ignore_errors=True)
+shutil.rmtree(testImageMasks, ignore_errors=True)
+shutil.rmtree(testImagePreds, ignore_errors=True)
+shutil.rmtree(testImageMorphology, ignore_errors=True)
+
+
+dirs = [testImagePreds, testImageMasks, testImage,
+        testImageMorphology, open_, erode, dilate, close]
+mkdir_list(dirs)
+
+ls = np.array(os.listdir(image))
+np.random.shuffle(ls)
+ls = ls[:15]
 for l in ls:
-    shutil.copy(f"image/{l}", "testImage")
-    shutil.copy(f"imageMasks/{l}", "testImageMasks")
+    shutil.move(os.path.join(image, l), testImage)
+    shutil.move(os.path.join(imageMask, l), testImageMasks)
 
 
 
